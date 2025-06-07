def6bcdc2115f47ad055e65112d9819d64b56c18
yoo chaerie <yoospam@protonmail.com>
2025-06-07 21:56:01 +0200

    chore: Renewing my dotfiles
    Will replace all dotfiles and move them into a seperate space then
    symlinking with gnu stow

--- nvim/lua/plugins/smearcursor.lua
+++
@@ -1,6 +0,0 @@
-return {
-  "sphamba/smear-cursor.nvim",
-  --- I like the defaults but it could be for example something snappier... look here:
-  --- https://github.com/sphamba/smear-cursor.nvim
-  opts = {},
-}
