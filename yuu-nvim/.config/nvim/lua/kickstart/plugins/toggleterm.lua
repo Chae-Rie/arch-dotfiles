22dc6a53f489cd4d62e442821235fc7211c38871
yoo chaerie <yoospam@protonmail.com>
2025-06-15 16:12:54 +0200

    chore(nvim): Adding lazygit and toggleterm
    Yet in the "wrong" directory --> inside the kickstart dir instead of
    customs but this can be adressed later.

---
+++ yuu-nvim/.config/nvim/lua/kickstart/plugins/toggleterm.lua
@@ -0,0 +1,17 @@
+return {
+  'akinsho/toggleterm.nvim',
+  version = '*',
+  cmd = 'ToggleTerm',
+  build = ':ToggleTerm',
+  keys = { { '<c-t>' } },
+  opts = {
+    size = 20,
+    open_mapping = [[<c-t>]],
+    direction = 'float',
+    terminal_mappings = true,
+    hide_numbers = true,
+    start_in_insert = true,
+    close_on_exit = true,
+    persist_size = true,
+  },
+}
