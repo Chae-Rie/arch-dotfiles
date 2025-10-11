c7948d7865000867420213d732c383c154db0b55
yoo chaerie <yoospam@protonmail.com>
2025-10-11 18:10:12 +0200

    !add(nvim): adding conditional shell choice
    I am annoyed of toggleterm not selecting the right shell so I query with
    vim functions for the os and so one for the given shell.
    On windows it probably gets the core-edition of powershell!

--- yuu-nvim/.config/nvim/lua/kickstart/plugins/toggleterm.lua
+++ yuu-nvim/.config/nvim/lua/kickstart/plugins/toggleterm.lua
@@ -1,3 +1,11 @@
+local function get_shell()
+  if vim.fn.has 'win32' == 1 or vim.fn.has 'win64' == 1 then
+    return 'pwsh.exe'
+  else
+    return '/usr/bin/zsh'
+  end
+end
+
 return {
   'akinsho/toggleterm.nvim',
   version = '*',
@@ -13,5 +21,6 @@ return {
     start_in_insert = true,
     close_on_exit = true,
     persist_size = true,
+    shell = get_shell(),
   },
 }
