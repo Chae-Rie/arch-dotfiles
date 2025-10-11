08c259c968815611ad4805e9c907570bfddbffea
yoo chaerie <yoospam@protonmail.com>
2025-10-11 18:40:13 +0200

    !add(nvim): Adding trouble.nvim for a visually more pleasing diagnostics window

--- yuu-nvim/.config/nvim/init.lua
+++ yuu-nvim/.config/nvim/init.lua
@@ -1005,8 +1005,8 @@ require('lazy').setup({
   require 'kickstart.plugins.smearcursor', -- fancy cursor animation
   require 'kickstart.plugins.rustaceanvim', -- a better rust analyzer
   require 'kickstart.plugins.cmaketools', -- for working with CMakefiles
-  -- require 'kickstart.plugins.gitsigns', -- adds gitsigns recommend keymaps
-
+  require 'kickstart.plugins.gitsigns', -- adds gitsigns recommend keymaps
+  require 'kickstart.plugins.trouble', -- for visually pleasing diagnostics, just like in your avg. IDE
   -- NOTE: The import below can automatically add your own plugins, configuration, etc from `lua/custom/plugins/*.lua`
   --    This is the easiest way to modularize your config.
   --
