55b151c657823b602df6df9f37564b64b8edd089
yoo chaerie <yoospam@protonmail.com>
2025-10-11 18:23:41 +0200

    add(nvim): Adding non closing executor window

--- yuu-nvim/.config/nvim/lua/kickstart/plugins/cmaketools.lua
+++ yuu-nvim/.config/nvim/lua/kickstart/plugins/cmaketools.lua
@@ -1,4 +1,12 @@
 return {
   'Civitasv/cmake-tools.nvim',
-  opts = {},
+  opts = {
+    cmake_executor = {
+      name = 'quickfix',
+      opts = {
+        auto_close_when_success = false, -- Should be kept open, so I can read the console
+        show = 'always',
+      },
+    },
+  },
 }
