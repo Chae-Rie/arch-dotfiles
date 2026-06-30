5d938ddf873c7186917c735ff224c4b9ffa9c89e
yoo chaerie <yoospam@protonmail.com>
2026-06-30 21:59:04 +0200

    change(hypr): Little changes on the hypr-lua config

--- lua-hypr/.config/hypr/hyprland.lua
+++ lua-hypr/.config/hypr/hyprland.lua
@@ -1,5 +1,23 @@
-# I just call the stuff I want similiar to my old config
+---
+--- H   H Y   Y PPPP  RRRR        RRRR   OOO   OOO  TTTTT
+--- H   H  Y Y  P   P R   R       R   R O   O O   O   T
+--- HHHHH   Y   PPPP  RRRR   ---  RRRR  O   O O   O   T
+--- H   H   Y   P     R  R        R  R  O   O O   O   T
+--- H   H   Y   P     R   R       R   R  OOO   OOO    T
+---
+---
+--- I just call the stuff I want similiar to my old config
+
+--- Monitor stuff
 require("source.monitors")
 
+---
 
-
+---  -------------------------------------------------
+--- I definetly need the following:
+--- - Program-autostart stuff
+--- - Keybindings
+--- - Windowrules
+--- - Inputs
+--- - Looks (pretty stuff)
+---
---
+++ lua-hypr/.config/hypr/source/monitors.lua
@@ -0,0 +1,15 @@
+--------------------
+-- -M   M  OOO  N   N III TTTTT  OOO  RRRR
+-- -MM MM O   O NN  N  I    T   O   O R   R
+-- -M M M O   O N N N  I    T   O   O RRRR
+-- -M   M O   O N  NN  I    T   O   O R  R
+-- -M   M  OOO  N   N III   T    OOO  R   R
+-- -
+--- FYI: Monitor got detected under HDMI-A-2
+
+hl.monitr({
+	output = "",
+	mode = "preferred",
+	position = "auto",
+	scale = 1,
+})
